6b5c24cc630cad228e8cd8fa692c05114fdc7eb2
Dave Ott <daveott@daveott.net>
2011-12-29 10:56:25 -0500

    Vimbundles shell script

---
+++ bin/vimbundles.sh
@@ -0,0 +1,30 @@
+mkdir -p ~/.vimbundles
+cd ~/.vimbundles
+
+get_bundle() {
+  (
+  if [ -d "$2" ]; then
+    echo "Updating $1's $2"
+    cd "$2"
+    git pull --rebase
+  else
+    git clone "git://github.com/$1/$2.git"
+  fi
+  )
+}
+
+get_bundle godlygeek tabular
+get_bundle kchmck vim-coffee-script
+get_bundle leshill vim-json
+get_bundle mileszs ack.vim
+get_bundle tpope vim-commentary
+get_bundle tpope vim-cucumber
+get_bundle tpope vim-eunuch
+get_bundle tpope vim-fugitive
+get_bundle tpope vim-haml
+get_bundle tpope vim-pathogen
+get_bundle tpope vim-rails
+get_bundle tpope vim-repeat
+get_bundle tpope vim-surround
+get_bundle tpope vim-unimpaired
+get_bundle vim-ruby vim-ruby
